e1c4ec4b37fbba9b1c22c641077586639a356e5f
Tamannatanwa <tamanna22@navgurukul.org>
2026-02-09 21:46:08 +0530

    sidebar work is in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path = "/login" element={<LoginPage />} />
